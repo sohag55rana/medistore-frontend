3cf6e4010cec6fbba0439be3b16adcbfbac8d7fc
sohag55rana <sohag55rana@gmail.com>
2026-06-19 20:24:09 +0600

    Add navbar and footer

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -174,7 +174,7 @@ export default function Navbar() {
                             className="flex items-center space-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 transition"
                           >
                             <ShoppingBag className="h-4 w-4 text-gray-400" />
-                            <span>Seller Orders</span>
+                            <span>Seller Order</span>
                           </Link>
                           <Link
                             href="/seller/add-medicine"
